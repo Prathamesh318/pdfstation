010e093ab99e3f3505bbdba364c2b6b37ef6596a
Prathamesh318 <ov01809@gmail.com>
2025-12-22 12:40:25 +0530

    Added pdfEntity Job for pdf processing

---
+++ src/main/java/com/app/pdfstation/entity/PdfJob.java
@@ -0,0 +1,45 @@
+package com.app.pdfstation.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "pdf_job")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PdfJob {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String operation; // COMPRESS, MERGE, SPLIT
+
+    @Column(nullable = false)
+    private String status; // CREATED
+
+    @Column(nullable = false)
+    private String inputPath;
+
+    private String outputPath;
+
+    private int retryCount;
+
+    private String errorMessage;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
+
