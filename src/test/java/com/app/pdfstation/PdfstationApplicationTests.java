f0ea79ebe63405a47d9501f54f55dc124d7e724b
Prathamesh318 <ov01809@gmail.com>
2025-12-22 12:34:53 +0530

    first commit

---
+++ src/test/java/com/app/pdfstation/PdfstationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.pdfstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfstationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
